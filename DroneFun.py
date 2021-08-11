c0520ff8115629f8d7c1a97be05fb50353d401fd
Leo Zhang <leotheasianlion@gmail.com>
2021-08-11 19:03:38 -0400

    updates

--- DroneFun.py
+++ DroneFun.py
@@ -54,7 +54,6 @@ global img
 
 #endregion
 
-
 #region keyboard controls
 
 # def init():
@@ -159,21 +158,13 @@ global img
 
 
 #region face tracking
+fbRange = [6200,6800]
+# change sensitivity of error by this value
+pid = [0.4,0.4,0]
+pError = 0
+w,h = 360,240
 
-def initCamera():
-
-    cap = cv2.VideoCapture(0)
-
-    while True:
-        _, img = cap,read()
-        img = cv2.resize(img, (w,h))
-        img,info = findFace(img ())
-        pError = trackFace(drone, info, w, pid, pError)
-        print('center:',info[0],'area:',info[1],)
-
-        cv2.imshow('video', img)
-
-        cv2.waitKey(1)
+#region find face()
 
 @enforce_types
 def findFace(img, color:tuple[int,int,int]=(0,0,255)):
@@ -183,8 +174,7 @@ def findFace(img, color:tuple[int,int,int]=(0,0,255)):
     -the center value will be used to rotate
     -the area value will be used to go forwards and backwards
     '''
-
-    faceCascade - cv2.CascadeClassifier('./Resources/haarcascades/haarcascade_frontalface_default.xml')
+    faceCascade = cv2.CascadeClassifier('./Resources/haarcascades/haarcascade_frontalface_default.xml')
     # converting to gray scale
 
     imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -216,12 +206,9 @@ def findFace(img, color:tuple[int,int,int]=(0,0,255)):
         return img, [faceLstCenters[i], faceLstArea[i]]
     
     return img, [[0,0],0]
+#endregion
 
-fbRange = [6200,6800]
-# change sensitivity of error by this value
-pid = [0.4,0.4,0]
-pError = 0
-w,h = 360,240
+#region track face()
 
 def trackFace(me, info, w, pid, pError):
     area = info[1]
@@ -232,6 +219,7 @@ def trackFace(me, info, w, pid, pError):
     
     speed = pid[0]*error + pid[1]*(error - pError)
     speed = int(np.clip(speed,-100,100))
+    fb = 0
     
     # keep drone stationary
     if fbRange[0] < area < fbRange[1]:
@@ -242,9 +230,40 @@ def trackFace(me, info, w, pid, pError):
     # else if face is too far, then move drone towards face
     elif area < fbRange[0] and area != 0:
         fb = 20
+        
+    print(error, fb)
+        
+    # if we don't get anything, then we have to stop
+    if x == 0:
+        speed = 0
+        error = 0
+        
+        
     # send command to drone
-    me.send_rc_control(0,fb,0,speed)
-    return pError
+    # me.send_rc_control(0,fb,0,speed)
+    return error
+#endregion
+
+#region initCamera
+
+def initCamera():
+    global pError,drone
+    cap = cv2.VideoCapture(0)
+    while True:
+        _, img = cap.read()
+        img = cv2.resize(img, (w,h))
+        
+        img,info = findFace(img)
+        pError = trackFace(drone, info, w, pid, pError)
+        print('center:',info[0],'area:',info[1],)
+
+        # display image
+        cv2.imshow('MyResult', img)
+        cv2.waitKey(1)
+            
+#endregion
+
+initCamera()
 
 #endregion
 
